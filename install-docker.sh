8271ebc608a4f7e58473dd6b8bf94f7b7f3994d0
zloi iglesias <daz@mm.st>
2020-03-19 13:45:40 +0300

    install docker

---
+++ install-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable"
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+
