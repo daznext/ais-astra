aac8d3418a10d8ae79f2960df1a6245b1a08b8c1
zloi iglesias <daz@mm.st>
2020-03-19 14:56:17 +0300

    fix

--- install-docker.sh
+++ install-docker.sh
@@ -2,7 +2,7 @@
 # wget https://github.com/daznext/ais-astra/raw/master/install-docker.sh -O - | /bin/bash -
 
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-commonsudo ttf-mscorefonts-installer
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common sudo ttf-mscorefonts-installer
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable"
 sudo apt-get update && sudo apt-get -y upgrade
