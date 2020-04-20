aaf47c716ff82d9d775c8f2c86560de6424235d1
daz <daz@mm.st>
2020-04-20 21:31:36 +0500

    CUPS fixes

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+service cups start && lpadmin -p cups-pdf -v cups-pdf:/ -E -P /usr/share/ppd/cups-pdf/CUPS-PDF_noopt.ppd
+
 # Copy and take ownership of .Xauthority
 if [ -f /root/.Xauthority ]; then
     cp /root/.Xauthority /home/wineuser
