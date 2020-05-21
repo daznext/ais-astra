b4faedf73630b789accf97053a187c14e2512079
daz <daz@mm.st>
2020-05-21 19:32:42 +0500

    old cs

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -2,6 +2,8 @@
 
 service cups start && lpadmin -p cups-pdf -v cups-pdf:/ -E -P /usr/share/ppd/cups-pdf/CUPS-PDF_noopt.ppd
 
+/opt/cprocsp/sbin/ia32/cryptsrv
+
 # Copy and take ownership of .Xauthority
 if [ -f /root/.Xauthority ]; then
     cp /root/.Xauthority /home/wineuser
